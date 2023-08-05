0e09581e4108f3847621c5d99fe00301abe96aa2
varenh <bvarenhorst@outlook.com>
2023-08-05 22:59:11 +0200

    added class for db

--- app.py
+++ app.py
@@ -1,8 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
-app = Flask (__name__)
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///db.sqlite"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    complete = db.Column(db.Boolean)
+    
 
 
 @app.route('/')
 def index():
-    return "What is up"
+    return render_template('base.html')
+
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
